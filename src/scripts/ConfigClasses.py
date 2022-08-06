3c8f1e475a1058d366dffae72a96c9472ca7b498
v.d.olennikov <v.d.olennikov@tcsbank.ru>
2022-08-06 14:01:02 +0300

    add choose

--- src/scripts/ConfigClasses.py
+++ src/scripts/ConfigClasses.py
@@ -1,35 +1,35 @@
 from dataclasses import dataclass
 from datetime import datetime
 
+@dataclass
+class BasedConfig:
+    list_allowed: list = None
+
 
 @dataclass
-class IntConfig:
+class IntConfig(BasedConfig):
     min_int_value: int = 0
     max_int_value: int = 1000000
-    list_allowed: list = None
 
 
 @dataclass
-class StrConfig:
+class StrConfig(BasedConfig):
     min_str_len: int = 5
     max_str_len: int = 100
-    list_allowed: list = None
 
 
 @dataclass
-class DatetimeConfig:
+class DatetimeConfig(BasedConfig):
     min_datetime: datetime = datetime(1988, 1, 1)
     max_datetime: datetime = datetime(2030, 12, 31)
     min_timestamp: float = 1659773975.6536
     max_timestamp: float = 1659774975.6536
-    list_allowed: list = None
 
 
 @dataclass
-class FloatConfig:
+class FloatConfig(BasedConfig):
     min_float_value: float = -100.0
     max_float_value: float = 100.0
-    list_allowed: list = None
 
 
 @dataclass
--- src/scripts/FieldValueGenerator.py
+++ src/scripts/FieldValueGenerator.py
@@ -4,20 +4,35 @@ from scripts.ConfigClasses import *
 
 
 def generate_int(intConfig: IntConfig) -> int:
+    if intConfig.list_allowed is not None:
+        return random.choice(intConfig.list_allowed)
+
     return random.randint(intConfig.min_int_value, intConfig.max_int_value)
 
 
 def generate_str(strConfig: StrConfig) -> str:
+    if strConfig.list_allowed is not None:
+        return random.choice(strConfig.list_allowed)
+
     return Faker(["ru_RU"]).text().replace(' ', '')[:random.randint(strConfig.min_str_len, strConfig.max_str_len)]
 
 
 def generate_datetime(datetimeConfig: DatetimeConfig) -> datetime:
+    if datetimeConfig.list_allowed is not None:
+        return random.choice(datetimeConfig.list_allowed)
+
     return Faker(['ru_RU']).date_between_dates(datetimeConfig.min_datetime, datetimeConfig.max_datetime)
 
 
 def generate_float(floatConfig: FloatConfig) -> float:
+    if floatConfig.list_allowed is not None:
+        return random.choice(floatConfig.list_allowed)
+
     return random.uniform(floatConfig.min_float_value, floatConfig.max_float_value)
 
 
 def generate_timestamp(datetimeConfig: DatetimeConfig) -> float:
+    if datetimeConfig.list_allowed is not None:
+        return random.choice(datetimeConfig.list_allowed)
+
     return random.uniform(datetimeConfig.min_timestamp, datetimeConfig.max_timestamp)
