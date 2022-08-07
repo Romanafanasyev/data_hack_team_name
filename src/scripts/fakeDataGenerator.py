99f7dc6cda9b1ea081eaf42bb039966dfa9e8d6a
romanafanasyev <rafanya.pvl@gmail.com>
2022-08-07 09:55:33 +0300

    joinable

--- src/scripts/fakeDataGenerator.py
+++ src/scripts/fakeDataGenerator.py
@@ -6,64 +6,61 @@ import pandas as pd
 from scripts.ConfigClasses import Config
 
 
+def generate_tables_as_list(tables_class_list, config_path=None):
+    # get user config
+    if config_path is not None:
+        with open(config_path, 'r') as user_config_json:
+            user_config_dict = json.load(user_config_json)
+    else:
+        user_config_dict = {}
+
+    table_list = []
+    for table_class in tables_class_list:
+
+        # update table config
+        config_dict = {**asdict(table_class.config), **user_config_dict[table_class.__name__]}
+
+        table_class.config = Config.get_from_dict(config_dict)
+        table = []
+        for _ in range(table_class.config.rows_number):
+            obj = table_class()
+            table.append(asdict(obj))
+
+        table_list.append(table)
+    return table_list
+
+
 class FakeDataGenerator:
 
     @staticmethod
     def gen_all(spark, tables_class_list, config_path=None):
 
-        # get user config
-        if config_path is not None:
-            with open(config_path, 'r') as user_config_json:
-                user_config_dict = json.load(user_config_json)
-        else:
-            user_config_dict = {}
+        tables_list = generate_tables_as_list(tables_class_list, config_path)
 
-        df_list = []
-        for table_class in tables_class_list:
-
-            # update table config
-            config_dict = {**asdict(table_class.config), **user_config_dict[table_class.__name__]}
-
-            table_class.config = Config.get_from_dict(config_dict)
-            table = []
-            for _ in range(table_class.config.rows_number):
-                obj = table_class()
-                table.append(asdict(obj))
-
+        spark_df_list = []
+        for table in tables_list:
             df = spark.createDataFrame(table)
-            df_list.append(df)
+            spark_df_list.append(df)
 
-        return df_list
+        return spark_df_list
 
     @staticmethod
     def gen_joinable(spark, tables_class_list, first_key_list, second_key_list, config_path=None):
 
-        if config_path is not None:
-            with open(config_path, 'r') as user_config_json:
-                user_config_dict = json.load(user_config_json)
-        else:
-            user_config_dict = {}
-
-        tables_list = []
-        for table_class in tables_class_list:
-
-            config_dict = {**asdict(table_class.config), **user_config_dict}
-
-            table_class.config = Config.get_from_dict(config_dict)
-            table = []
-            for _ in range(table_class.config.rows_number):
-                obj = table_class()
-                table.append(obj)
-
-            # df = spark.createDataFrame(table)
-            tables_list.append(table)
+        tables_list = generate_tables_as_list(tables_class_list, config_path)
 
         # Изменения для джойнов
         for row in range(len(tables_list[0])):
             for col_num in range(len(first_key_list)):
                 tables_list[1][row].__dict__[second_key_list[col_num]] = tables_list[0][row].__dict__[
                     first_key_list[col_num]]
-        return tables_list
+
+        spark_df_list = []
+        for table in tables_list:
+            df = spark.createDataFrame(table)
+            spark_df_list.append(df)
+
+        return spark_df_list
 
     @staticmethod
     def gen_from_file(spark, table_class, file_path):
